babc8d3fa5ed6236a08fb24444a9d818bacd87a8
chengyong <willcy1006@163.com>
2019-02-24 15:09:44 +0800

    completed url detection

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, jsonify
+from flask import Flask, render_template, request, url_for, jsonify, redirect
 from flask_bootstrap import Bootstrap
 
 import os
@@ -17,10 +17,40 @@ def index():
 @app.route('/url-detect', methods=["GET",'POST'])
 def predict():
     if request.method == "GET":
-        return render_template('url_input.html')
-    if request.method == 'POST':
-        pass
+        return render_template('url-input.html')
 
+    req = request.values
+    resp = {"code":200, 'msg':"提交成功","data":{}}
+    url = req['url'] if 'url' in req else ''
+
+
+    if not url:
+        resp['code'] = -1
+        resp['msg'] = "请输入符合格式的网址"
+        return jsonify(resp)
+
+    url_vector = vectorizer.transform([url])
+    predicted = model.predict(url_vector)
+    app.logger.error(predicted)
+    link = url_for('result', url=url, result=predicted[0])
+    resp['link'] = str(link)
+    return jsonify(resp)
+    #return jsonify(resp)
+    #return render_template('url-detect-result.html')
+
+@app.route('/url-detect-result')
+def result():
+    req = request.values
+    resp_data = {}
+    result_id = req['result'] if 'result' in req else ''
+    test_link = req['url'] if 'url' in req else ''
+    if not result_id or not test_link:
+        return redirect('/')
+
+    resp_data['test_link'] = test_link
+    resp_data['result_id'] = result_id
+
+    return render_template('url-detect-result.html',test_link=test_link, result_id=result_id)
 
 # namequery = request.form['namequery']
 # X_test_sparse_matrix = count_vect.transform([namequery])
@@ -31,7 +61,8 @@ def predict():
 # return render_template('results.html',prediction = my_prediction,name = namequery.upper())
 
 if __name__ == '__main__':
-    vectorizer = joblib.load('./machine_learning_model/url_detection/my_model.pkl')
+    from sklearn.externals import joblib
+    vectorizer = joblib.load('./machine_learning_model/url_detection/vectroizer.pkl')
     model = joblib.load('./machine_learning_model/url_detection/my_model.pkl')
 
     app.run(debug=True)
--- static/assets/js/url_detect/index.js
+++ static/assets/js/url_detect/index.js
@@ -1,16 +1,59 @@
+;
+var url_detect_ops = {
+    init: function () {
+        this.eventBind();
+        this.resizeContent();
+    },
 
-$(document).ready(function(){
+    eventBind: function () {
+        var that = this;
+        $(".url_detect_wrap .submit").click(function () {
+            var btn_target = $(this);
+            if (btn_target.hasClass("disable")) {
+                alert("正在處理，請勿重複提交");
+                return;
+            }
 
-    resizeContent();
+            var url_input = $("#url_input").val();
+            //console.log(url_input);
+            if (url_input == null || url_input.length<3){
+                alert("请输入大于3个字符的内容");
+                return false;
+            }
 
-    $(window).resize(function() {
-        resizeContent();
+            $.ajax({
+                url:'/url-detect',
+                type:"POST",
+                data:{"url":url_input},
+                dataType:'json',
+                success:function (res) {
+                    btn_target.removeClass("disable");
+                    var callback = null;
+                    if (res.code == 200) {
+                         //callback = function () {
+                             window.location.href = "http://127.0.0.1:5000"+res.link;
+                         //}
+                        //alert(res.msg, callback);
+                    }
+
+                }
+            });
+        })
+    },
+
+    resizeContent: function() {
+        $height = $(window).height() - 250;
+        $('#page-frame').height($height);
+    }
+};
+
+
+$(document).ready(function () {
+
+    url_detect_ops.init();
+    $(window).resize(function () {
+        url_detect_ops.resizeContent();
     });
-    
-   $('.services-item').matchHeight();
+
 });
 
-function resizeContent() {
-    $height = $(window).height() -250;
-     $('#page-frame').height($height);
-}
